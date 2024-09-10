cb682de65800ca09b977d9cf2e91d7d9ce91099c
Jairo Andrés Portela Cortés <jairo.portela@imagineapps.co>
2024-09-09 21:37:45 -0500

    feat 🆕: add cat breeds repository to handle the logic with search

---
+++ packages/cat_breeds_repository/lib/src/cat_breeds_repository.dart
@@ -0,0 +1,34 @@
+import 'package:the_cat_api/the_cat_api.dart';
+
+/// {@template cat_breeds_repository}
+/// A repository that handles catbreed related requests.
+/// {@endtemplate}
+class CatBreedsRepository {
+  /// {@macro cat_breeds_repository}
+  const CatBreedsRepository({
+    required TheCatApiConstructor api,
+  }) : _api = api;
+  final TheCatApiConstructor _api;
+
+  ///Get cat breeds with `search` optional
+  Future<List<CatBreed>> getCatBreeds(String? search) async {
+    try {
+      final query = search != null
+          ? _api.getCatBreedsBySearch(search)
+          : _api.getCatBreeds();
+      final result = await query;
+      if (search != null && result.isEmpty) throw CatBreedsNotFoundFailure();
+      return result;
+    } on CatBreedsNotFoundFailure {
+      rethrow;
+    } catch (error) {
+      throw CatBreedsRequestFailure();
+    }
+  }
+}
+
+/// Exception thrown when getCatBreeds fails.
+class CatBreedsRequestFailure implements Exception {}
+
+/// Exception thrown when cat breeds for provided search are not found.
+class CatBreedsNotFoundFailure implements Exception {}
