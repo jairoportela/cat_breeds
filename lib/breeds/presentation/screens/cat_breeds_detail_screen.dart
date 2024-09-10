851a728a4cf107d4a2cfdbd053c25e64959865a0
Jairo Andrés Portela Cortés <jairo.portela@imagineapps.co>
2024-09-09 22:10:54 -0500

    fix 🔨: add widget import

--- lib/breeds/presentation/screens/cat_breeds_detail_screen.dart
+++ lib/breeds/presentation/screens/cat_breeds_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cat_breeds/breeds/data/models/cat_breed.dart';
+import 'package:cat_breeds/breeds/presentation/widgets/cat_breed_datail_info.dart';
 import 'package:flutter/material.dart';
 
 class CatBreedDetail extends StatelessWidget {
@@ -24,49 +25,6 @@ class CatBreedDetail extends StatelessWidget {
               child: CatBreedDatailInfo(
             breed: breed,
           )),
-        ],
-      ),
-    );
-  }
-}
-
-class CatBreedDatailInfo extends StatefulWidget {
-  const CatBreedDatailInfo({super.key, required this.breed});
-  final CatBreedModel breed;
-  @override
-  State<CatBreedDatailInfo> createState() => _CatBreedDatailInfoState();
-}
-
-class _CatBreedDatailInfoState extends State<CatBreedDatailInfo> {
-  final ScrollController _controller = ScrollController();
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scrollbar(
-      thumbVisibility: true,
-      trackVisibility: true,
-      controller: _controller,
-      child: ListView(
-        controller: _controller,
-        padding: const EdgeInsets.all(20),
-        children: [
-          Text(widget.breed.description),
-          const SizedBox(height: 10),
-          Text('Nombre pais: ${widget.breed.countryName}'),
-          if (widget.breed.intelligence != null) ...[
-            const SizedBox(height: 10),
-            Text('Inteligencia: ${widget.breed.intelligence}')
-          ],
-          if (widget.breed.adaptability != null) ...[
-            const SizedBox(height: 10),
-            Text('Adaptabilidad: ${widget.breed.adaptability}')
-          ]
         ],
       ),
     );
